d3722126254e4eb6b53d894fd5f47c93e51c9712
Chisel <chisel@chizography.net>
2011-07-13 22:02:44 +0100

    Initial commit

---
+++ profiles/default/Module.pm
@@ -0,0 +1,14 @@
+package {{$name}};
+# ABSTRACT: {{$name}} needs a more meaningful abstract
+use strict;
+use warnings;
+
+=head1 NAME
+
+{{$name}}
+
+=cut
+ 
+1;
+__END__
+# vim: ts=8 sts=4 et sw=4 sr sta
