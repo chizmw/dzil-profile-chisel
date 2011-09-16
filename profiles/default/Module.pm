83b6a99bbbf2a9644beb7622e4e3706910260b36
Chisel <chisel@chizography.net>
2011-09-16 09:16:42 +0100

    Updates to dzil profile
     - module skeleton has ABSTRACT at the end
     - we no longer use NextVersion
       - prefer Git::NextVersion

--- profiles/default/Module.pm
+++ profiles/default/Module.pm
@@ -1,8 +1,8 @@
 package {{$name}};
-# ABSTRACT: {{$name}} needs a more meaningful abstract
 use strict;
 use warnings;
 
 1;
+# ABSTRACT: {{$name}} needs a more meaningful abstract
 __END__
 # vim: ts=8 sts=4 et sw=4 sr sta
